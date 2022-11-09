e093c556d78270b4c46e52d9cfde410c8287df4a
Seth <seth.bbrendle@gmail.com>
2022-11-09 12:52:57 -0500

    added show action

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -4,4 +4,13 @@ class PlantsController < ApplicationController
         render json: Plant.all, status: :ok 
     end
 
+    def show
+        plant = Plant.find_by(id:params[:id])
+        if plant
+            render json: plant, status: :ok
+        else
+            render json: {error: "Plant not found"}, status: :not_found
+        end
+    end
+
 end
